0feabc8a44739ce7a3252fd939d7d6f6396aa653
novis10813 <zhizhongz895253@gmail.com>
2022-05-15 13:36:43 +0800

    finish

---
+++ Brand/BrandClassifier.py
@@ -0,0 +1,36 @@
+import torch.nn as nn
+
+from transformers import BertModel
+
+class ThresholdClassifier(nn.Module):
+    def __init__(self, freeze=True):
+        super(ThresholdClassifier, self).__init__()
+        self.bert_model = BertModel.from_pretrained('bert-base-chinese')
+        self.classifier = nn.Sequential(
+            nn.Linear(768, 512),
+            nn.ReLU(),
+            nn.Linear(512, 256),
+            nn.ReLU(),
+            nn.Linear(256, 121)    
+        )
+        
+        if freeze:
+            for p in self.bert_model.parameters():
+                p.requires_grad = False
+    
+    def forward(self, input_ids, attention_mask):
+        x = self.bert_model(input_ids, attention_mask)
+        # print(x[0].shape) (batch, max_length, 768)
+        # print(x[1].shape) (batch, 768)
+        x = self.classifier(x[1])
+        # distribution = torch.softmax(x)
+        return x # distribution
+
+
+# def weights_init(m):
+#     classname = m.__class__.__name__
+#     if classname.find('Conv') != -1:
+#         nn.init.normal_(m.weight, 0.0, 0.02)
+#     elif classname.find('BatchNorm') != -1:
+#         nn.init.normal_(m.weight, 1.0, 0.02)
+#         nn.init.zeros_(m.bias)
---
+++ Brand/DataPreprocess.py
@@ -0,0 +1,32 @@
+import torch
+from torch.utils.data import Dataset
+
+
+class BrandDataset(Dataset):
+    def __init__(self, X, y, tokenizer, max_length):
+        self.X = X
+        self.y = y
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+    
+    def __getitem__(self, index):
+        text = self.X[index]
+        inputs = self.tokenizer.encode_plus(
+            text,
+            None,
+            add_special_tokens=True,
+            max_length=self.max_length,
+            pad_to_max_length=True,
+            return_token_type_ids=True
+        )
+        ids = inputs['input_ids']
+        mask = inputs['attention_mask']
+        
+        return {
+            'ids': torch.tensor(ids, dtype=torch.long),
+            'mask': torch.tensor(mask, dtype=torch.long),
+            'targets': torch.tensor(self.y[index], dtype=torch.float32)
+        }
+    
+    def __len__(self):
+        return len(self.X)
